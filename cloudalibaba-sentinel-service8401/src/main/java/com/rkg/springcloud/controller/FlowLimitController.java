e164e2877cf7f560e53f2bdc285386f5d86a06e1
renkaige <renkaige499388564@qq.com>
2020-06-29 22:31:12 +0800

    20200629添加sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/rkg/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,29 @@
+package com.rkg.springcloud.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+public class FlowLimitController
+{
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+
+        return "------testB";
+    }
+
+
+
+}
+ 
+ 
